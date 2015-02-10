2835b6e3751f0a2da027a3b37b784a6084699f4b
Jen Best <jenthebest@gmail.com>
2015-02-10 11:38:56 -0500

    Updated js

--- script.js
+++ script.js
@@ -16,11 +16,63 @@ var view = {
 	}
 };
 
+/* Testing
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
+*/
+
 var model = {
-	boardSize
-	numShips
-	ships
-	shipsSunk
-	shipLength
-	fire
-}
+	boardSize: 7,
+	numShips: 3,
+	shipsSunk: 0,
+	shipLength: 3,
+	ships: [{locations: ["06", "16", "26"], hits: ["", "", ""] },
+			{locations: ["24", "34", "44"], hits: ["", "", ""] },
+			{locations: ["10", "11", "12"], hits: ["", "", ""] }],
+	fire: function(guess) {
+		for (var i = 0; i < this.numShips; i++) {
+			var ship = this.ships[i];
+			var index = ship.locations.indexOf(guess);
+			if (index >= 0) {
+				// We have a hit!
+				ship.hits[index] = "hit";
+				view.displayHit(guess);
+				view.displayMessage("HIT!");
+				if (this.isSunk(ship)) {
+					view.displayMessage("You sank my battleship!");
+					this.shipsSunk++;
+				}
+				return true;
+			}
+		}
+		// We don't have a hit
+		view.displayMiss(guess);
+		view.displayMessage("You missed.");
+		return false;
+	},
+	isSunk: function(ship) {
+		for (var i = 0; i < this.shipLength; i++) {
+			if (ship.hits[i] !== "hit") {
+				return false;
+			}
+		}
+		return true;
+	}
+};
+
+/* Testing
+model.fire("53");
+model.fire("06");
+model.fire("16");
+model.fire("26");
+model.fire("34");
+model.fire("24");
+model.fire("44");
+model.fire("12");
+model.fire("11");
+model.fire("10");
+*/
